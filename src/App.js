ab66e5f9d3dae11491c302c521215645003534e0
gbmahou1 <gbmahou1@hotmail.com>
2022-05-11 18:39:20 -0300

    1.0

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+      </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
